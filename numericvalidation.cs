3cbf4d07b9889bab64f523212961b494f5f42381
roshantetartagonist <55144377+roshantetartagonist@users.noreply.github.com>
2019-10-08 13:51:57 +0100

    Add files via upload

---
+++ numericvalidation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace and.or
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Please enter a number between '0' & '10':");
+            number = int.Parse(Console.ReadLine());
+
+            if ((number <= 10) || (number >= 0))
+                Console.WriteLine("Well done!");
+            else
+                Console.WriteLine("Hey! Please enter a number between '0' and '10'!");
+            
+          
+        }
+    }
+}
